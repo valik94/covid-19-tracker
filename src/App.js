5b019c3218a582a3f6d82ec3d46dd58277d1a6a6
Val Hustle <stars@MacBook-Air.local>
2021-12-30 00:52:02 -0500

    added table.js ad table.css to style the live cases by country table as well as updated the pulling of data in the app.js using getCountry and getCounryInfo State variables

--- src/App.js
+++ src/App.js
@@ -1,69 +1,111 @@
+import React, { useState, useEffect } from "react";
+import { MenuItem, FormControl, Select, Card, CardContent } from "@material-ui/core";
+import InfoBox from "./InfoBox";
+import Map from "./Map";
+import Table from "./Table";
 
-import React, {useState, useEffect} from 'react';
-import {MenuItem,FormControl,Select} from "@material-ui/core";
-import InfoBox from './InfoBox';
-import './App.css';
+import "./App.css";
 
 function App() {
-  const [countries, setCountries] = useState(['USA', 'UK','India']);
-  const [country, setCountry] = useState('worldwide');
+  const [countries, setCountries] = useState(["USA", "UK", "India"]);
+  const [country, setCountry] = useState("worldwide");
+  const [countryInfo, setCountryInfo] = useState([])
+  const [tableData, setTableData] = useState([])
+
+  useEffect(()=>{
+    fetch('https://disease.sh/v3/covid-19/all')
+    .then(response => response.json())
+    .then(data =>{
+      setCountryInfo(data);
+    })
+  }, [])
+
+
 
   //STATE = How to write a variable in REACT<<
 
   //UseEffect = Runs a piece of code based on a given condition
-  useEffect(()=>{
+  useEffect(() => {
     //async -> send request, await for it, do something with it
-    const getCountriesData = async () =>{
-      await fetch ('https://disease.sh/v3/covid-19/countries')
-      .then((response)=> response.json())
-      .then((data)=>{ 
-         const countries = data.map((country)=> (
-          {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
             name: country.country, //United States, United Kingdom, etc
             value: country.countryInfo.iso2, //UK, USA, FR
           }));
+          setTableData(data);
           setCountries(countries);
-      })
-    }
+        });
+    };
     getCountriesData();
   }, []);
 
-  const onCountryChange = (event) =>{
+  const onCountryChange = async (event) => {
     const countryCode = event.target.value;
-    console.log('COUNTRY CODE IS >>>', countryCode);
-    setCountry (countryCode);
-  }
-   
+    console.log("COUNTRY CODE IS >>>", countryCode);
+    setCountry(countryCode);
+     
+
+    const url = countryCode === 'worldwide' ? `https://disease.sh/v3/covid-19/all` : `https://disease.sh/v3/covid-19/countries/${countryCode}`
+
+    await fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      setCountry(countryCode); //All the data
+      setCountryInfo(data); //All the data from the country response
+    })
+    //Worldwide stats https://disease.sh/v3/covid-19/all
+    //countries states https://disease.sh/v3/covid-19/countries
+  };
+  console.log("COUNTRY INFO >>>", countryInfo)
+  
   return (
-    <div className="app"> 
-    <div className="app_header">
-    <h1>COVID 19 TRACKER</h1>
-    <FormControl className="app__dropdown">
-      <Select variant="outlined" onChange={onCountryChange} value={country} >
-        {/* Loop through all the countries 
+    <div className="app">
+      <div className="app_left">
+        <div className="app_header">
+          <h1>COVID 19 TRACKER</h1>
+          <FormControl className="app__dropdown">
+            <Select
+              variant="outlined"
+              onChange={onCountryChange}
+              value={country}
+            >
+              {/* Loop through all the countries 
         and show dropdown list of countries */}
-        <MenuItem value="worldwide">Worldwide</MenuItem>
-        {countries.map((country) =>(
-            <MenuItem value={country.value}>{country.name}</MenuItem>
-          ))}
-         
-          {/* <MenuItem value="worldwide">Worldwide</MenuItem>
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+
+              {/* <MenuItem value="worldwide">Worldwide</MenuItem>
           <MenuItem value="worldwide">Canada</MenuItem>
           <MenuItem value="worldwide">United States</MenuItem>
           <MenuItem value="worldwide">Ukraine</MenuItem> */}
-      </Select>
-    </FormControl>
-    </div>
+            </Select>
+          </FormControl>
+        </div>
 
-   <div className = "app_status">
-     <InfoBox title="Coronavirus cases" cases={123} total={2000} />
-     <InfoBox title="Recovered"cases={1234}  total={3000}/>
-     <InfoBox title="Deaths" cases={12345} total={4000}/>
-      
-      </div>  
-      {/* Table */}
-      {/* Graph */}
-      {/* Map */}
+        <div className="app_status">
+          <InfoBox title="Coronavirus cases" cases={countryInfo.todayCases} total={countryInfo.cases} />
+          <InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered} />
+          <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths} />
+        </div>
+
+        <Map />
+      </div>
+      <div>
+      <Card className="app_right">
+        <CardContent>
+        {/* Graph */}
+
+        <h3>Live Cases by Cuntry</h3>
+        <Table countries={tableData} />
+        <h3>Wordlwide New cases</h3>
+        </CardContent>
+      </Card>
+      </div>
     </div>
   );
 }
